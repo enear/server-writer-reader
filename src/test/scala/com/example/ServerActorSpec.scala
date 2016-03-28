3dcb4a471460911857ae1c594fb6ad4efb119ac5
João Ferreira <joao.ferreira@enear.co>
2016-03-28 16:55:23 +0100

    handle recovery completion

--- src/test/scala/com/example/ServerActorSpec.scala
+++ src/test/scala/com/example/ServerActorSpec.scala
@@ -24,15 +24,18 @@ class ServerActorSpec extends TestKit(ActorSystem("ServerActorSpec")) with Impli
   writerActor.setAutoPilot(new TestActor.AutoPilot {
   def run(sender: ActorRef, msg: Any): TestActor.AutoPilot =
     msg match {
-      case RequestData(1, 10) ⇒ {
-        (1 to 10) foreach { i => sender ! WriterData(i) }
+      case RequestData(offset, length) ⇒ {
+        (1 to length).foldLeft(offset) { (accum, next) =>
+          sender ! WriterData(accum)
+          accum + 1
+        }
         TestActor.KeepRunning
       }
     }
 })
  
   "A Server actor" must {
-    val serverActor = system.actorOf(ServerActor.props)
+    var serverActor = system.actorOf(ServerActor.props)
 
     "receive greets" in {
       writerActor.send(serverActor, WriterGreet)
@@ -57,5 +60,29 @@ class ServerActorSpec extends TestKit(ActorSystem("ServerActorSpec")) with Impli
       readerActor.expectNoMsg()
     }
     
+    "handle restarts" in {
+      val id3 = UUID.randomUUID()
+      readerActor.send(serverActor, ReaderRequest(id3, 0))
+      
+      serverActor ! Kill
+      
+      serverActor = system.actorOf(ServerActor.props)
+      writerActor.send(serverActor, WriterGreet)
+      
+      val id4 = UUID.randomUUID()
+      readerActor.send(serverActor, ReaderRequest(id4, 0))
+      val updates3 = readerActor.receiveN(9) map (_.asInstanceOf[SequenceUpdate])
+      updates3 map(_.count) shouldBe sorted
+      updates3 map(_.uuid) should contain only id3
+      readerActor.expectMsg(500.millis, SequenceUpdate(id3, -1))
+      readerActor.send(serverActor, RemoveId(id3))
+      
+      val updates4 = readerActor.receiveN(10) map (_.asInstanceOf[SequenceUpdate])
+      updates4 map(_.count) shouldBe sorted
+      updates4 map(_.uuid) should contain only id4
+      readerActor.expectMsg(500.millis, SequenceUpdate(id4, -1))
+      readerActor.send(serverActor, RemoveId(id4))
+    }
+
   }
 }
