30cae687aa462bfe6dafc570962919f815a2e722
João Ferreira <joao.ferreira@enear.co>
2016-03-28 15:49:07 +0100

    test ServerActor reader message starvation

--- src/test/scala/com/example/ServerActorSpec.scala
+++ src/test/scala/com/example/ServerActorSpec.scala
@@ -2,7 +2,7 @@ package com.example
 
 import java.util.UUID
 
-import akka.actor.ActorSystem
+import akka.actor._
 import akka.testkit._
 import com.example.ReaderActor.SequenceUpdate
 import com.example.ServerActor._
@@ -21,27 +21,41 @@ class ServerActorSpec extends TestKit(ActorSystem("ServerActorSpec")) with Impli
 
   val readerActor = TestProbe()
   val writerActor = TestProbe()
+  writerActor.setAutoPilot(new TestActor.AutoPilot {
+  def run(sender: ActorRef, msg: Any): TestActor.AutoPilot =
+    msg match {
+      case RequestData(0, 10) ⇒ {
+        (0 to 9) foreach { i => sender ! WriterData(i) }
+        TestActor.KeepRunning
+      }
+    }
+})
  
   "A Server actor" must {
     val serverActor = system.actorOf(ServerActor.props)
 
     "receive greets" in {
       writerActor.send(serverActor, WriterGreet)
-//      serverActor.underlyingActor.writerActor shouldEqual Some(writerActor.ref)
     }
 
     "start a sequence counting correctly" in {
       val id1 = UUID.randomUUID()
       val id2 = UUID.randomUUID()
       readerActor.send(serverActor, ReaderRequest(id1, 0))
-      readerActor.send(serverActor, ReaderRequest(id2, 0))
       writerActor.expectMsg(5000.millis, RequestData(0, 10))
-      (0 to 9) foreach { i => writerActor.send(serverActor, WriterData(i)) }
-      val updates = readerActor.receiveN(10) map (_.asInstanceOf[SequenceUpdate])
-      updates map(_.count) shouldBe sorted
+      val updates1 = readerActor.receiveN(10) map (_.asInstanceOf[SequenceUpdate])
+      updates1 map(_.count) shouldBe sorted
       readerActor.expectMsg(500.millis, SequenceUpdate(id1, -1))
       readerActor.send(serverActor, RemoveId(id1))
-//      serverActor.underlyingActor.currentState shouldEqual Some(id2, 0)
+      
+      readerActor.send(serverActor, ReaderRequest(id2, 0))
+      val updates2 = readerActor.receiveN(10) map (_.asInstanceOf[SequenceUpdate])
+      updates2 map(_.count) shouldBe sorted
+      readerActor.expectMsg(500.millis, SequenceUpdate(id2, -1))
+      readerActor.send(serverActor, RemoveId(id2))
+
+      readerActor.expectNoMsg()
     }
+    
   }
 }
